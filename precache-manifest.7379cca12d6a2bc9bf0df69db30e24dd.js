9054523b3366591cd49e7edb5526d18481b98ed2
KKCrypto <kristijan.kaloczi@gmail.com>
2020-08-10 09:08:06 +0200

    Updates

---
+++ precache-manifest.7379cca12d6a2bc9bf0df69db30e24dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f611383a8e8455729093ae08e3714460",
+    "url": "/react-crypto-search/index.html"
+  },
+  {
+    "revision": "ce71c680e09cd346e02f",
+    "url": "/react-crypto-search/static/css/2.318eb5a6.chunk.css"
+  },
+  {
+    "revision": "e8c17ef2202b512d8786",
+    "url": "/react-crypto-search/static/css/main.ec37db6f.chunk.css"
+  },
+  {
+    "revision": "ce71c680e09cd346e02f",
+    "url": "/react-crypto-search/static/js/2.5ef68cfa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-crypto-search/static/js/2.5ef68cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c17ef2202b512d8786",
+    "url": "/react-crypto-search/static/js/main.fa697308.chunk.js"
+  },
+  {
+    "revision": "5476906a91537c39c0d0",
+    "url": "/react-crypto-search/static/js/runtime-main.00bf82ef.js"
+  }
+]);
